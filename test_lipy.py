76d39e563ef1854c9c55f704b50ecfacddaf7587
berkio3x <kshitij.singh@neotas.com>
2021-05-23 19:46:48 +0530

    Fix: add suport for string with spaces
    Make symbol & string seperate

--- test_lipy.py
+++ test_lipy.py
@@ -2,26 +2,41 @@ from main import execute_program
 
 
 progs =[
-    (
-    '''
-    ( begin
-        (define x 10)
-        (set! x (+ x 10))
-        (* x 2))
-    ''',
-    40),
+   
+    ('''(+ 100 200)''', 300),
+    ('''(- 2 2)''',   0),
+    ('''(* 3 2 2)''', 12),
+    ('''(/ 6 2)''',   3),
+    ('''(if (> 1 2) (+ 2 2) (* 2 3))''',   6),
+    ('''(if (> 3 2) (+ 1 2) (* 2 3))''',   3),
+    ('''(begin (define x 10) (set! x (+ 2 x)) (+ 1 x))''',13),
+    ('''(define x 10)''', None),
     
-    ('''
-    (begin 
-        (define amount (lambda (r) (* 2 (* r r))))
-        (amount 20)
-    )
-    ''',
-    800)
+    
+    # String 
+    
+    ('''(print "hello")''','"hello"'),
+    ('''(print "hello world")''','"hello world"'),
+    
+    #Lists
+    #('''(cons 1 2)''', [1,2]),
+    ('''(car (list 1 2 3 4))''', 1),
+    ('''(car (cdr (list 1 2 3 4)))''', 2),
+    ('''(car (cdr (cdr (list 1 2 3 4))))''', 3),
+    ('''(cdr (list 1 2 3 4))''', [2, 3, 4]),
+    ('''(begin (define x (list 1 2 3 4)) (list-ref x 2))''',3),
+    ('''(list 1 2 (list 3 4))''',[1, 2, [3, 4]]),
+
+
+    # Procedures
+    ('''((lambda (x) (+ x x)) 10)''',20),
+    ('''(begin (define amount (lambda (r) (* 2 (* r r)))) (amount 20))''',800)
 ]
     
     
 if __name__ == "__main__":
     for prog in progs:
         source, output = prog
-        assert execute_program(source, file=False) == output
+        out = execute_program(source, file=False)
+        assert out == output
+        print(f'Test: {source} => {out}','......OK')
